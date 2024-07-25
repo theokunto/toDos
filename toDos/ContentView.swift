dcc3813f0112b81ec291acfbfe0369f0c5a04b81
theokunto <theofilus.business@gmail.com>
2024-07-26 01:17:17 +0700

    Initial Commit

---
+++ toDos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toDos
+//
+//  Created by Theofilus Kuntolukito on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toDos/toDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDosApp.swift
+//  toDos
+//
+//  Created by Theofilus Kuntolukito on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct toDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
